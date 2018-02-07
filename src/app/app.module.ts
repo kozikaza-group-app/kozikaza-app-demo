0eeba744dcabef5374fc26eabaecc5a42e19a3b1
Huynh Huu My <my.huynh@sutrixsolutions.com>
2018-02-07 13:49:47 +0700

    create register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { RegisterPage } from './../pages/register/register';
+import { RegisterComponent } from './../components/register/register';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +19,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    RegisterPage,
+    TabsPage,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +33,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    RegisterPage,
+    TabsPage,
+    RegisterComponent
   ],
   providers: [
     StatusBar,
